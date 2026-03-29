b0804361a4a808bf69d2822e3d4b28fe63f66d2c
sannysuarez <sannysuarez4@gmail.com>
2026-03-29 12:10:12 +0100

    fixeddd

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from .config import config
+from config import config
 import os
 
 db = SQLAlchemy()
